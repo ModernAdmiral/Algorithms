aca17237397e31fef03b3310efb6165d615c807e
Jackson McComas <jackson.mccomas@gmail.com>
2020-03-18 16:28:23 -0500

    got it phew

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,39 @@
 
 import argparse
 
-def find_max_profit(prices):
-  pass
 
+def find_max_profit(prices):
+    # loop though prices
+    # second loop though the last part of prices, with i.
+    # subtract i from j, storing the max_profit_so_far
+    # contain max_profit_so_far and current_min_price_so_far
+    # record the biggest differece between the two
+    max_profit_so_far = -99999
+    # current_min_price_so_far = 0
+    for i in range(len(prices)):
+        print("prices[i]", prices[i:])
+        if len(prices[i:]) > 0:
+            for j in range(i, len(prices) - 1):
+                if j == i:
+                    j += 1
+                if prices[j] - prices[i] > max_profit_so_far:
+                    max_profit_so_far = prices[j] - prices[i]
+
+    return max_profit_so_far
+
+
+# print(find_max_profit([1050, 270, 1540, 3800, 2]))
+
+# print(find_max_profit([100, 90, 80, 50, 20, 10]))
+# print(find_max_profit([10, 7, 5, 8, 11, 9]))
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
